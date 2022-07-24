27663c968b9939fd82e100223c07cfc77fad3854
Kacper <k.trzepiecinski@gmail.com>
2022-07-25 00:12:05 +0200

    table fix

--- src/components/Tabel.js
+++ src/components/Tabel.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function Table(props) {
   const dataForTable = props.data;
-  const keys = ["ID", "Hash", "Datetime", "From", "Method", "To", "Quantity"];
+  const keys = ["ID", "Method", "From", "Quantity", "Hash", "Datetime", "To"];
 
   return (
     <table className="content-table">
